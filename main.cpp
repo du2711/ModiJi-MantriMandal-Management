443884c233afbb228460df1d140239824fab4d6c
du2711 <deepanshuupadhyay603@gmail.com>
2026-08-12 18:15:43 +0530

    added deletion function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<fstream>
 #include<sstream>
+#include<cstdio>
 
 using namespace std;
 
@@ -102,7 +103,7 @@ void displayEmployee(){
         getline(ss,temp,',');
         employee.salary=stoi(temp);
 
-        cout << employee.empId << "\t"
+        cout <<employee.empId << "\t"
             << employee.name << "\t"
             << employee.deptId << "\t\t"
             << employee.dept << "\t\t"
@@ -110,6 +111,70 @@ void displayEmployee(){
     }
 
     ptr.close();
+}
+
+void deleteEmployee(){
+    ifstream ptr("employee.csv",ios::in);
+
+    if(!ptr) {
+        cout<<"Error in opening file";
+        return;
+    }
+
+    emp employee;
+    ofstream temp("temp.csv",ios::out);
+
+    if(!temp){
+        cout<<"Error in opening file";
+        return; 
+    }
+
+    int del;
+    cout<<"Enter Employee Id to delete: ";
+    cin>>del;
+
+    string line;
+    string tempData;
+    bool found = false;
+
+    while(getline(ptr,line)){
+        stringstream ss(line);
+
+        getline(ss,tempData,',');
+        employee.empId=stoi(tempData);
+
+        getline(ss,employee.name,',');
+
+        getline(ss,tempData,',');
+        employee.deptId=stoi(tempData);
+
+        getline(ss,employee.dept,',');
+
+        getline(ss,tempData,',');
+        employee.salary=stoi(tempData);
+
+        if(employee.empId != del){
+            temp <<employee.empId << ","
+            << employee.name << ","
+            << employee.deptId << ","
+            << employee.dept << ","
+            << employee.salary << "\n";
+        }
+        else found=true;
+    }
+
+    ptr.close();
+    temp.close();
+
+    if(found){
+        cout<<"Employee Deleted Successfully";
+        remove("employee.csv");
+        rename("temp.csv","employee.csv");
+    }
+    else {
+        cout<<"Employee not found";
+        remove("temp.csv");
+    }
 }
 
 int main(){ 
