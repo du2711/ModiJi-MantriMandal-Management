e265bc475045f612c1dda59f2ee73195d5cf416e
du2711 <deepanshuupadhyay603@gmail.com>
2026-08-17 17:21:56 +0530

    Added update employee feature

--- main.cpp
+++ main.cpp
@@ -177,6 +177,105 @@ void deleteEmployee(){
     }
 }
 
+void updateEmployee(){
+    ifstream ptr("employee.csv");
+
+    if(!ptr){
+        cout<<"Error in opening file";
+        return;
+    }
+
+    emp employee;
+
+    int edit;
+    cout<<"Enter Employee Id: ";
+    cin>>edit;
+
+    ofstream temp("temp.csv");
+
+    if(!temp){
+        cout<<"Error in opening file";
+        return;
+    }
+
+    string line;
+    string tempdata;
+    bool found = false;
+
+    deptName depName;
+
+    while(getline(ptr,line)){
+        stringstream ss(line);
+
+        getline(ss,tempdata,",");
+        employee.empId=stoi(tempdata);
+
+        getline(ss,employee.name,",");
+
+        getline(ss,tempdata,",");
+        employee.deptId=stoi(tempdata);
+
+        getline(ss,employee.dept,",");
+
+        getline(ss,tempdata,",");
+        employee.salary=stoi(tempdata);
+
+        if(edit==employee.empId){
+            found=true;
+            cout<<"Enter Employee name: ";
+            cin>>employee.name;
+
+            cout<<"Enter Department ID: ";
+            cin>>employee.deptId;
+
+            switch(employee.deptId){
+                case 1011:
+                    employee.dept=depName.IT;
+                    break;
+                case 1012:
+                    employee.dept=depName.Sales;
+                    break;
+                case 1013:
+                    employee.dept=depName.HR;
+                    break;
+                case 1014:
+                    employee.dept=depName.Finance;
+                    break;
+                case 1015:
+                    employee.dept=depName.Marketing;
+                    break; 
+                default:
+                    cout<<"Enter valid Department ID";
+                    ptr.close();
+                    temp.close();
+                    remove("temp.csv");
+                    return;
+            }
+            cout<<"Enter Salary: ";
+            cin>>employee.salary;
+        }
+
+        temp<< employee.empId << ","
+            << employee.name << ","
+            << employee.deptId << ","
+            << employee.dept << ","
+            << employee.salary << "\n";
+    }
+
+    ptr.close();
+    temp.close();
+
+    if(found){
+        remove("employee.csv");
+        rename("temp.csv","employee.csv");
+        cout<<"Employee Details Updated Successfully";
+    }
+    else{
+        remove("temp.csv");
+        cout<<"Employee not found";
+    }
+}
+
 int main(){ 
     int choice;
     while(1){
